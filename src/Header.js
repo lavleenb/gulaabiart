0d90d02f2126cbffb908ca07974e2cafe30fa67e
lavleen <lavleenbhachu4@gmail.com>
2025-08-15 12:13:00 -0400

    adding style

--- src/Header.js
+++ src/Header.js
@@ -5,16 +5,8 @@ export default function Header() {
     return (
         <header>
             <h1>
-                Gulaabi Art
+                GULAABI ART
             </h1>
-            <nav>
-                <ul>
-                    <li> <a href="/">Home</a></li>
-                    <li> <a href="/">Projects</a></li>
-                    <li> <a href="/">Artwork</a></li>
-                    <li> <a href="/">Contact</a></li>
-                </ul>
-            </nav>
         </header>
     );
 }
